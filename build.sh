31c373ee4ccc3d50db22741b1543060af0dc3c42
Lu <117615336+lu-developer476@users.noreply.github.com>
2026-07-13 23:30:33 -0300

    Ensure Render build creates runtime virtualenv (#5)

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
+python -m venv .venv
+. .venv/bin/activate
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
 python manage.py collectstatic --no-input
